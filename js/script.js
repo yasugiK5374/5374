8b27b0ad56ed30eca42bd414f82b03ad30242af0
matsueT5374 <mishima001@users.noreply.github.com>
2018-02-28 14:32:57 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ var cblankEndDD = 3;
 
 //var zurasi = '１月の収集日は１週ずれます。'
 
-// ◇  var 今日 = new Date('2019/01/24');
+// ◇  var 今日 = new Date('2019/01/30');
 
 
 /**
@@ -125,7 +125,7 @@ var TrashModel = function(_lable, _cell, remarks, transferdata) {
 
   var result_text = "";
   //◇ var today = new Date();
-  var today = new Date('2019/01/24');
+  var today = new Date('2019/01/30');
 
   for (var j in this.dayCell) {
     if (this.dayCell[j].length == 1) {
@@ -214,7 +214,7 @@ var TrashModel = function(_lable, _cell, remarks, transferdata) {
     if (this.regularFlg == 1) {
 
       // ◇ var today = new Date();
-      var today = new Date('2019/01/24');
+      var today = new Date('2019/01/30');
 
       // 12月 +3月　を表現
       for (var i = 0; i < MaxMonth; i++) {
@@ -310,7 +310,7 @@ var TrashModel = function(_lable, _cell, remarks, transferdata) {
     })
     //直近の日付を更新
     // ◇ var now = new Date();
-    var now = new Date('2019/01/24');
+    var now = new Date('2019/01/30');
 
     // ◇ 上に移動した
     //this.bikohyoji = "";
@@ -605,6 +605,10 @@ $(function() {
   }
 
   function createAreaSelect() {
+    
+
+
+
     var $select_area = $('#select_area');
     var $select_group = $('#select_group');
     var select_html = "";
@@ -674,7 +678,7 @@ $(function() {
     var group = areaGroup[group_name];
     var areaModel = group[area_name];
     // ◇ var today = new Date();
-    var today = new Date('2019/01/24');
+    var today = new Date('2019/01/30');
 
     window.alert('日付◇' + today);
 
