38eb25552eb8672f5b99f81a059815ccf5dda7cc
matsueT5374 <mishima001@users.noreply.github.com>
2018-02-28 11:52:54 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ var cblankEndDD = 3;
 
 var zurasi = '１月の収集日は１週ずれます。'
 
-// ◇  var 今日 = new Date('2018/12/31');
+// ◇  var 今日 = new Date('2019/01/01');
 
 
 /**
@@ -125,7 +125,7 @@ var TrashModel = function(_lable, _cell, remarks, transferdata) {
 
   var result_text = "";
   //◇ var today = new Date();
-  var today = new Date('2018/12/31');
+  var today = new Date('2019/01/01');
 
   for (var j in this.dayCell) {
     if (this.dayCell[j].length == 1) {
@@ -214,7 +214,7 @@ var TrashModel = function(_lable, _cell, remarks, transferdata) {
     if (this.regularFlg == 1) {
 
       // ◇ var today = new Date();
-      var today = new Date('2018/12/31');
+      var today = new Date('2019/01/01');
 
       // 12月 +3月　を表現
       for (var i = 0; i < MaxMonth; i++) {
@@ -310,7 +310,7 @@ var TrashModel = function(_lable, _cell, remarks, transferdata) {
     })
     //直近の日付を更新
     // ◇ var now = new Date();
-    var now = new Date('2018/12/31');
+    var now = new Date('2019/01/01');
 
     // ◇ 上に移動した
     //this.bikohyoji = "";
@@ -674,7 +674,7 @@ $(function() {
     var group = areaGroup[group_name];
     var areaModel = group[area_name];
     // ◇ var today = new Date();
-    var today = new Date('2018/12/31');
+    var today = new Date('2019/01/01');
 
     window.alert('日付◇' + today);
 
