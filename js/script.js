32da66e85c7f5c4802e02fd623d3db6a7e83cef3
matsueT5374 <mishima001@users.noreply.github.com>
2019-03-20 16:55:57 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ var AreaModel = function() {
   */
   this.isBlankDay = function(currentDate,startKDate) {
   
-    alert("なか①☆" + startKDate);
+    alert("なか①" + startKDate);
 
     // center.csv の期間のチェック
     if (this.startDate.length > 0) {
@@ -51,14 +51,16 @@ var AreaModel = function() {
     
     alert("なか②" + tuki);
     
+    var endYear;
+    
     // 固定期間チェック　休止終了日は開始日の次の年
     // ※ 20190320 休止開始が１２月のみ終了年を+1する
     if (tuki == 11 ) {
         alert("なか③○");
-        var endYear = startKDate.getFullYear() + 1;
+        endYear = startKDate.getFullYear() + 1;
     } else {
         alert("なか③×");
-        var endYear = startKDate.getFullYear();
+        endYear = startKDate.getFullYear();
     }
     end
     
