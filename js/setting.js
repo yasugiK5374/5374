5d447b9b48b103ace91217f79add4f22ebe9b33c
matsueT5374 <mishima001@users.noreply.github.com>
2019-03-26 16:19:21 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -7,7 +7,7 @@ var MaxDescription = 9; // ごみの最大種類、９を超えない場合は�
 var MaxMonth = 3;
 
 var KoteiKN = false;    // 固定の休止期間１２月３０日～１月３日を使用するときはtrue。
-var WeekShiftA = false; // 休止期間なら週をずらすときは、true。金沢の仕様は、true。
-var WeekShiftB = true; // 休止期間なら週をずらすときは、true。金沢の仕様は、true。
+var WeekShiftA = false; // 休止期間なら週をずらすときは、true。Ａ地区
+var WeekShiftB = true;  // 休止期間なら週をずらすときは、true。Ｂ地区
 
 var SkipSuspend = true; // 休止期間を除去するときは、true。奈良の仕様は、true。
